b56addee4e834ed5e4287c5ad4982f51b9319259
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-04-04 11:16:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
+      st.header('Luis Carlos, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+      st.header('Vacio, con Probabilidad: '+str( prediction[0][1]))
